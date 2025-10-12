dcb5ada55762517d75dfd73a0d14d40c3e942696
ermBlood <bloodcze@gmail.com>
2025-10-12 19:41:38 +0200

    added detection for countdown bar

--- klik.py
+++ klik.py
@@ -17,10 +17,19 @@ STOP_ALL = Event()
 STOP_LOOP = Event()
 MODE = "get_input"
 POS_MODE = "AUTO"
+INTERVAL_MODE = "AUTO"
+
+ARROW_POS = None
 
 
+def look_for_stop():
+    if STOP_ALL.is_set():
+        exit()
+    if STOP_LOOP.is_set():
+        return True
 
-def loop_click(interval, pos_and_scale):
+
+def loop_click(pos_and_scale, interval):
     if pos_and_scale == None:
         return
     pos, scale = pos_and_scale
@@ -32,15 +41,17 @@ def loop_click(interval, pos_and_scale):
     while True:
         for i in range(interval):
             time.sleep(1)
-            if STOP_ALL.is_set():
-                exit()
-            if STOP_LOOP.is_set():
-                STOP_LOOP.clear()
+            if look_for_stop():
                 return
+            time.sleep(.1)
             if POS_MODE == "AUTO" and not is_arrow_still_there(pos, scale):
                 print("Tlačítko nenalezeno")
                 return
-            
+            while is_waiting_bar() == True:
+                time.sleep(.1)
+        
+        if look_for_stop():
+            return
         print("klik")
         mouse.position = pos
         mouse.click(Button.left)
@@ -49,6 +60,8 @@ def loop_click(interval, pos_and_scale):
 
 
 def do_repeat():
+    global STOP_LOOP
+    STOP_LOOP.clear()
     global MODE
     MODE = "get_input"
     global POS_MODE
@@ -72,19 +85,25 @@ def exit():
 
 
 def get_interval():
-    global MODE
-    while True:
-        clear_input()
-        MODE = "get_input"
-        interval = input("Zadej interval kliků (v sekundách, rozmezí 1-99): ")
-        try:
-            interval = round(float(interval))
-            if 0 < interval < 100:
-                return interval
-            else:
-                print("..erm, celé číslo v rozmezí od nuly, do devadesáti-devíti :)")
-        except:
-            print("Číslo, ty jelito")
+    global INTERVAL_MODE
+    if INTERVAL_MODE == "MANUAL":
+ 
+        global MODE
+        while True:
+            clear_input()
+            MODE = "get_input"
+            interval = input("Zadej interval kliků (v sekundách, rozmezí 1-99): ")
+            try:
+                interval = round(float(interval))
+                if 0 < interval < 100:
+                    return interval
+                else:
+                    print("..erm, celé číslo v rozmezí od nuly, do devadesáti-devíti :)")
+            except:
+                print("Číslo, ty jelito")
+
+    if INTERVAL_MODE == "AUTO":
+        return 1
 
 
 def get_pos_manual():
@@ -168,6 +187,8 @@ def get_arrow_pos():
                 arrow_y = int(screen_result[3][1] + resized_arrow.shape[0]/2)
                 screen_arrow_pos = (arrow_x, arrow_y)
                 print(f"Pozice tlačítka je {screen_arrow_pos}")
+                global ARROW_POS
+                ARROW_POS = screen_arrow_pos
                 return screen_arrow_pos, scale
                     
         global MODE
@@ -181,8 +202,6 @@ def get_arrow_pos():
 
 
 def is_arrow_still_there(pos, scale):
-    print(pos)
-    print(scale)
     arrow = cv2.imread("arrow.png")
     w = arrow.shape[1]
     h = arrow.shape[0]
@@ -201,13 +220,51 @@ def is_arrow_still_there(pos, scale):
         return screen_match[1] > .7
 
 
+def is_waiting_bar():
+    global ARROW_POS
+    if ARROW_POS == None:
+        return False
+    x, y = ARROW_POS
+
+    time.sleep(.5)
+ 
+    with mss.mss() as sct:
+        screenshot = sct.grab({"left": x-150, "top": y, "width": 150, "height": 30})
+        screen = numpy.array(screenshot)
+        screen = screen[:, :, :3]
+
+        # cv2.imshow("lol", screen)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        time.sleep(.5)
+
+        with mss.mss() as sct2:
+            screenshot2 = sct2.grab({"left": x-150, "top": y, "width": 150, "height": 30})
+            screen2 = numpy.array(screenshot2)
+            screen2 = screen2[:, :, :3]
+
+            # cv2.imshow("lo2l", screen2)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+
+            diff = cv2.absdiff(screen, screen2)
+            if diff.mean() == 0:
+                return False
+            else:
+                if look_for_stop():
+                    return False
+                print("Čekám na bar")
+                return True
+
+
 def main():
 
     keyboard_listener()
     print("\nVítej v utilitě Klik3000\n")
 
     while not STOP_ALL.is_set():
-        loop_click(get_interval(), get_arrow_pos())
+        loop_click(get_arrow_pos(), get_interval())
         do_repeat()
 
 main()
